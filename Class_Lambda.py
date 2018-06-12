4030df52573fcd494b93ad20e22cea18872f0170
Holstiche <matthijs.toa@gmail.com>
2018-06-12 12:40:24 +0200

    begonnen met definieren class

---
+++ Class_Lambda.py
@@ -0,0 +1,31 @@
+#denk na over invoer van functies zonder haakjes (alfa_reductie)
+
+def replace(lijst, old, new):
+	for i in range(len(lijst)):
+		if lijst[i] == old:
+			lijst[i]=new
+	return lijst
+
+class functie:
+	def __init__(self, pram, body):
+		self.pram=pram
+		self.body=body
+
+	def __str__(self):
+		pass
+
+	def alfa_redu(self):
+		pass
+
+	def beta_redu(self, argumenten):
+		while len(argumenten)>0:
+			self.body = replace(self.body, self.pram.pop(0), argumenten.pop(0))
+		'''for i in range(len(self.body)):
+			if len(self.body[i]>1):'''
+
+		return self.body
+
+functie = functie(pram=["a", "b", "c"],body=["a", "b", "c"])
+print(functie.beta_redu(["(lb.xy)", "(lsz.z)", "z"]))
+
+
