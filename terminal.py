154c896bc4e4d3f891f3c2e9840272ffba38b68e
Holstiche <matthijs.toa@gmail.com>
2018-06-21 16:54:41 +0200

    begin implementatie input naar lambda expressie

--- terminal.py
+++ terminal.py
@@ -1,10 +1,14 @@
+from str_to_expr import str_to_expr
+
+#welke invoer is wel geldig en welke niet
+
 input_user=""
 while input_user != "exit":
 	input_user = input()
 	print(input_user)
-	'''
-	lambda_versie_input = ...
+	lambda_versie_input = str_to_expr(input_user)
 	print(lambda_versie_input)
+	'''
 	gereduceerde_lambda_versie = ...
 	print(gereduceerde_lambda_versie)
 	uitkomst = ...
